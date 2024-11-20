052e9cc36c2c2791cb2b8bc77da439b7d0ffc127
Alexey Masasin <alexeymasasin@gmail.com>
2024-11-20 12:22:52 +0500

    React Router integration

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,35 @@
+import { Link } from "react-router-dom";
 import Button from "../ui/Button/Button";
 import styles from "./Header.module.css";
 
 export default function Header() {
     return (
         <header className={styles.wrapper}>
-            <div className={styles.item}>
-                <h2>Alexey Masasin</h2>
-            </div>
+            <Link to="/">
+                <div className={styles.item}>
+                    <img
+                        src="https://avatars.githubusercontent.com/u/101698179?v=4"
+                        alt="img"
+                    />
+                    <h2>Alexey Masasin</h2>
+                </div>
+            </Link>
             <nav className={styles.item}>
                 <ul className={styles.nav_list}>
-                    <li className={styles.nav_item}>Home</li>
-                    <li className={styles.nav_item}>About</li>
-                    <li className={styles.nav_item}>Projects</li>
-                    <li className={styles.nav_item}>Resume</li>
+                    <li className={styles.nav_item}>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className={styles.nav_item}>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li className={styles.nav_item}>
+                        <Link to="/project">Project</Link>
+                    </li>
+                    <li className={styles.nav_item}>
+                        <Link to="/resume">Resume</Link>
+                    </li>
                 </ul>
-                <Button weight="bold" uppercase={true}>
+                <Button weight="bold" uppercase={false}>
                     Contact
                 </Button>
             </nav>
