7a036fed70a187d77a86fbb2b4dba1ebf6857221
Alexey Masasin <alexeymasasin@gmail.com>
2024-11-22 15:04:15 +0500

    Gradient-bg component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,15 @@
 import Header from "./header/Header";
 import IntroBanner from "./sections/intro-banner/IntroBanner";
+import GradientBackground from "./ui/GradientBackground/GradientBackground";
 
 function App() {
-    return (
-        <div className="container">
-            <Header />
-            <IntroBanner />
-        </div>
-    );
+  return (
+    <div className="container">
+      <Header />
+      <IntroBanner />
+      <GradientBackground />
+    </div>
+  );
 }
 
 export default App;
