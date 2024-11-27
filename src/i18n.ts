e9cab6800a3c40f7233eb9932326a11352316a9e
Alexey Masasin <alexeymasasin@gmail.com>
2024-11-28 00:30:32 +0500

    gh-pages fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,21 +1,21 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import en from "./translation/en.json";
-import ru from "./translation/ru.json";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './translation/en.json';
+import ru from './translation/ru.json';
 
 const resources = {
-    en: {
-        translation: en,
-    },
-    ru: {
-        translation: ru,
-    },
+	en: {
+		translation: en,
+	},
+	ru: {
+		translation: ru,
+	},
 };
 
 i18n.use(initReactI18next).init({
-    resources,
-    lng: JSON.parse(localStorage.getItem("language")),
-    fallbackLng: "en",
+	resources,
+	lng: JSON.parse(localStorage.getItem('language') || '{}'),
+	fallbackLng: 'en',
 });
 
 export default i18n;
