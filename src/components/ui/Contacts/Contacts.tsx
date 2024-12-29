eb3e6716a4d30b3334db6194c0a57f2875f62025
Alexey Masasin <alexeymasasin@gmail.com>
2024-12-30 02:58:23 +0500

    WhatsApp button added

--- src/components/ui/Contacts/Contacts.tsx
+++ src/components/ui/Contacts/Contacts.tsx
@@ -1,43 +1,48 @@
-import { useTranslation } from "react-i18next";
-import { BiLogoTelegram } from "react-icons/bi";
-import { BsGithub } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
-import { PiPhoneCallFill } from "react-icons/pi";
-import Divider from "../Divider/Divider";
-import styles from "./Contacts.module.css";
-import ContactsForm from "./ContactsForm";
-import ContactsItem from "./ContactsItem";
+import { useTranslation } from 'react-i18next';
+import { BiLogoTelegram } from 'react-icons/bi';
+import { BsGithub, BsWhatsapp } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md';
+import { PiPhoneCallFill } from 'react-icons/pi';
+import Divider from '../Divider/Divider';
+import styles from './Contacts.module.css';
+import ContactsForm from './ContactsForm';
+import ContactsItem from './ContactsItem';
 
 export default function Contacts() {
-    const { t } = useTranslation();
+	const { t } = useTranslation();
 
-    return (
-        <div className={styles.wrapper}>
-            <h3>{t("CONTACT")}</h3>
-            <ContactsForm />
-            <Divider />
-            <div className={styles.items}>
-                <ContactsItem
-                    href="mailto:alexeymasasin@gmail.com"
-                    title="Email"
-                    icon={<MdEmail />}
-                />
-                <ContactsItem
-                    href="https://t.me/alexeymasasin"
-                    title="Telegram"
-                    icon={<BiLogoTelegram />}
-                />
-                <ContactsItem
-                    href="tel:+79999059020"
-                    title={t("PHONE")}
-                    icon={<PiPhoneCallFill />}
-                />
-                <ContactsItem
-                    href="https://github.com/alexeymasasin"
-                    title="Github"
-                    icon={<BsGithub />}
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.wrapper}>
+			<h3>{t('CONTACT')}</h3>
+			<ContactsForm />
+			<Divider />
+			<div className={styles.items}>
+				<ContactsItem
+					href="mailto:alexeymasasin@gmail.com"
+					title="Email"
+					icon={<MdEmail />}
+				/>
+				<ContactsItem
+					href="https://t.me/alexeymasasin"
+					title="Telegram"
+					icon={<BiLogoTelegram />}
+				/>
+				<ContactsItem
+					href="https://wa.me/79999059020"
+					title="WhatsApp"
+					icon={<BsWhatsapp />}
+				/>
+				<ContactsItem
+					href="tel:+79999059020"
+					title={t('PHONE')}
+					icon={<PiPhoneCallFill />}
+				/>
+				<ContactsItem
+					href="https://github.com/alexeymasasin"
+					title="Github"
+					icon={<BsGithub />}
+				/>
+			</div>
+		</div>
+	);
 }
